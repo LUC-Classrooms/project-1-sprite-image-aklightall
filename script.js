71eb845f61672cd803b52f0f6749411e12b9c9a2
aklightall <156939550+aklightall@users.noreply.github.com>
2024-02-22 04:08:38 +0000

    panda face complete

--- script.js
+++ script.js
@@ -4,48 +4,57 @@
  * Comments: 
  */
 
-// Global Variables go here
+let pandaX = 200; // x coordinate
+let pandaY = 200; // y coordinate
 
 function setup(){
   // this function will run once
-  createCanvas(600, 400); // create a 600x400 pixel drawing canvas
+  createCanvas(400, 400); // create a 400x400 pixel drawing canvas
 
 }
 
 function draw(){
   // this function runs again and again (60x per second)
-  background(200); //light gray background
+  background(0, 100, 0); // green background
+
+  translate(width / 2, height / 2); // center of canvas
   
-  // Body
-  fill(255); // white color
-  ellipse(270,300,200,200);
-  ellipse(270,200,150,150);
-  ellipse(270,100,100,100);
+  // Face
+  fill (225); // white color
+  ellipse (0, 0, 150, 150); // ellipse face
 
   // Eyes
-  fill(0); // black color
-  ellipse(250,90,10,10)
-  ellipse (290,90,10,10)
+  fill (0); // black eyes
+  ellipse (-25, -25, 30, 30); // left eye
+  ellipse (25, -25, 30, 30); // right eye
+
+  // Pupil
+  fill (255); // white pupil
+  ellipse (-20, -20, 10, 10); // right eye
+  ellipse (20, -20, 10, 10); // left eye
+
+  // Ears
+  fill (0); // black ears
+  ellipse (-70, -70, 50, 50); // left ear
+  ellipse (70, -70, 50, 50); // right ear
 
   // Nose
-  fill(250,150,0); // orange color 
-  triangle(270, 110, 270, 120, 320, 115)
-
-  // Buttons
-  fill(0); //  black color
-  ellipse(270,300,10,10);
-  ellipse(270,230,10,10);
-  ellipse(270,180,10,10);
-  
+  fill (0); // black nose 
+  ellipse (0, 20, 20, 20)
 
   // Mouth
   noFill();
-  stroke(0);
-  arc(270,125,70,15,0,PI)
+  stroke (0); // black color
+  arc (0, 40, 80, 40, 0, PI)
 
+  // Line from Nose to Mouth
+  stroke (0); // black color 
+  line (0, 20, 0, 60); // 
 
-  
-
+  // Blush on Cheeks
+  fill (255, 192, 203); // pink color
+  ellipse (-40, 10, 20, 10); // left blush
+  ellipse (40, 10, 20, 10); // right blush
 
 /* 
   Use the following if()...else() structure to incorporate mouse click control of your animation
